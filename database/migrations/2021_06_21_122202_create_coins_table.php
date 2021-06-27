bd21666e6862c66e6dd48081db8c2d69c8e5ba36
danielmordi <danielmordi.codes@gmail.com>
2021-06-27 04:41:18 -0500

    Modified coins_table_migration: id

--- database/migrations/2021_06_21_122202_create_coins_table.php
+++ database/migrations/2021_06_21_122202_create_coins_table.php
@@ -14,7 +14,7 @@ class CreateCoinsTable extends Migration
     public function up()
     {
         Schema::create('coins', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name')->unique();
             $table->string('wallet_id');
             $table->timestamps();
